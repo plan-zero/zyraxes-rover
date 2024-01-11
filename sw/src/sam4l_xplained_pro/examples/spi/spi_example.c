51d8a40172d08aeb1121de122b0fbe4e761acd06
amestereaga <mestereaga.andrei@gmail.com>
2024-01-11 21:00:51 +0200

    [SW] Add 4MHZ SPI config

--- sw/src/sam4l_xplained_pro/examples/spi/spi_example.c
+++ sw/src/sam4l_xplained_pro/examples/spi/spi_example.c
@@ -214,7 +214,7 @@ static uint32_t gs_ul_test_block_number;
 
 /* SPI clock configuration. */
 static const uint32_t gs_ul_clock_configurations[] =
-		{ 50000, 1000000, 2000000, 5000000 };
+		{ 50000, 1000000, 2000000, 4000000 };
 
 
 /**
